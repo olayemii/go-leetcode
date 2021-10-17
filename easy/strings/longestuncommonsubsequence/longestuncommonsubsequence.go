2edf5e4760cc083ad4073baf9c629497804342d8
Olayemii <donyemisco@gmail.com>
2021-10-17 10:01:23 +0100

    Solved 521. Longest Uncommon Subsequence I

---
+++ easy/strings/longestuncommonsubsequence/longestuncommonsubsequence.go
@@ -0,0 +1,54 @@
+/*
+521. Longest Uncommon Subsequence I
+
+https://leetcode.com/problems/longest-uncommon-subsequence-i/
+
+Given two strings a and b, return the length of the longest uncommon subsequence between a and b. If the longest uncommon subsequence does not exist, return -1.
+
+An uncommon subsequence between two strings is a string that is a subsequence of one but not the other.
+
+A subsequence of a string s is a string that can be obtained after deleting any number of characters from s.
+
+For example, "abc" is a subsequence of "aebdc" because you can delete the underlined characters in "aebdc" to get "abc". Other subsequences of "aebdc" include "aebdc", "aeb", and "" (empty string).
+
+
+Example 1:
+
+Input: a = "aba", b = "cdc"
+Output: 3
+Explanation: One longest uncommon subsequence is "aba" because "aba" is a subsequence of "aba" but not "cdc".
+Note that "cdc" is also a longest uncommon subsequence.
+Example 2:
+
+Input: a = "aaa", b = "bbb"
+Output: 3
+Explanation: The longest uncommon subsequences are "aaa" and "bbb".
+Example 3:
+
+Input: a = "aaa", b = "aaa"
+Output: -1
+Explanation: Every subsequence of string a is also a subsequence of string b. Similarly, every subsequence of string b is also a subsequence of string a.
+
+
+Constraints:
+
+1 <= a.length, b.length <= 100
+a and b consist of lower-case English letters.
+*/
+
+package main
+func findLUSlength(a string, b string) int {
+    lenA, lenB := len(a), len(b)
+    if (a == b) {
+        return -1
+    }else
+    if lenA > lenB {
+        return lenA
+    }else{
+        return lenB        
+    }
+    
+}
+
+// Space Complexity: O(1)
+// Time Complexity: O(1)
