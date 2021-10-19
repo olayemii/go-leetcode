8ca62cbbfd13862edd2b601606c4360b9de932c9
Olayemii <donyemisco@gmail.com>
2021-10-19 23:24:54 +0100

    Solved 806. Number of Lines To Write String

---
+++ easy/strings/numberoflinestowritestring/numberoflinestowritestring.go
@@ -0,0 +1,63 @@
+package main
+
+/*
+806. Number of Lines To Write String
+
+https://leetcode.com/problems/number-of-lines-to-write-string/
+
+You are given a string s of lowercase English letters and an array widths denoting how many pixels wide each lowercase English letter is. Specifically, widths[0] is the width of 'a', widths[1] is the width of 'b', and so on.
+
+You are trying to write s across several lines, where each line is no longer than 100 pixels. Starting at the beginning of s, write as many letters on the first line such that the total width does not exceed 100 pixels. Then, from where you stopped in s, continue writing as many letters as you can on the second line. Continue this process until you have written all of s.
+
+Return an array result of length 2 where:
+
+result[0] is the total number of lines.
+result[1] is the width of the last line in pixels.
+
+
+Example 1:
+
+Input: widths = [10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], s = "abcdefghijklmnopqrstuvwxyz"
+Output: [3,60]
+Explanation: You can write s as follows:
+abcdefghij  // 100 pixels wide
+klmnopqrst  // 100 pixels wide
+uvwxyz      // 60 pixels wide
+There are a total of 3 lines, and the last line is 60 pixels wide.
+Example 2:
+
+Input: widths = [4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10], s = "bbbcccdddaaa"
+Output: [2,4]
+Explanation: You can write s as follows:
+bbbcccdddaa  // 98 pixels wide
+a            // 4 pixels wide
+There are a total of 2 lines, and the last line is 4 pixels wide.
+
+
+Constraints:
+
+widths.length == 26
+2 <= widths[i] <= 10
+1 <= s.length <= 1000
+s contains only lowercase English letters.
+*/
+
+
+func numberOfLines(widths []int, s string) []int {
+	sum, currentLine, currentWidth := 0, 1, 0
+
+	for _, item := range s {
+        currentWidth = widths[item-'a']
+        if sum + currentWidth > 100 {
+            currentLine++
+			sum = 0
+        }
+		sum += currentWidth
+	}
+    answer := []int{currentLine, sum}
+	return answer;
+}
+
+
+// Space Complexity: 0(1)
+//  Time Complexity: O(n)
