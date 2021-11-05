099b821a87f0ea5d22755718d8d6f853ede80632
Olayemii <donyemisco@gmail.com>
2021-11-05 09:11:36 +0100

    Solved 1374. Generate a String With Characters That Have Odd Counts

---
+++ easy/strings/oddcountcharacterstring/oddcountcharacterstring.go
@@ -0,0 +1,56 @@
+/*
+1374. Generate a String With Characters That Have Odd Counts
+
+https://leetcode.com/problems/generate-a-string-with-characters-that-have-odd-counts/
+
+Given an integer n, return a string with n characters such that each character in such string occurs an odd number of times.
+
+The returned string must contain only lowercase English letters. If there are multiples valid strings, return any of them.
+
+
+
+Example 1:
+
+Input: n = 4
+Output: "pppz"
+Explanation: "pppz" is a valid string since the character 'p' occurs three times and the character 'z' occurs once. Note that there are many other valid strings such as "ohhh" and "love".
+Example 2:
+
+Input: n = 2
+Output: "xy"
+Explanation: "xy" is a valid string since the characters 'x' and 'y' occur once. Note that there are many other valid strings such as "ag" and "ur".
+Example 3:
+
+Input: n = 7
+Output: "holasss"
+
+
+Constraints:
+
+1 <= n <= 500
+*/
+package main
+
+func generateTheString(n int) string {
+    var biggestOdd int;
+    ans := "";
+    if n % 2 == 0 {
+        biggestOdd = n - 1
+    }else{
+        biggestOdd = n
+    }
+    i := 0;
+    for i < biggestOdd {
+        ans += "a"
+        i++;
+    }
+    
+    if n == biggestOdd {
+        return ans;
+    }
+    
+    return ans + "b"
+}
+
+// Space Complexity: O(n)
+// Time Complexity: O(n)
