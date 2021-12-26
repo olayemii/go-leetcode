914f60ae71e6874b10986bd5879cd76ac8406fe2
olayemii <donyemisco@gmail.com>
2021-12-26 12:24:46 +0100

    update solution

--- easy/linkedlist/deletenode/deletenode.go
+++ easy/linkedlist/deletenode/deletenode.go
@@ -46,12 +46,8 @@ type ListNode struct {
 }
 
 func deleteNode(node *ListNode) {
-	for node.Next.Next != nil {
-		node.Val = node.Next.Val
-		node = node.Next
-	}
 	node.Val = node.Next.Val
-	node.Next = nil
+	node.Next = node.Next.Next
 }
 
 // Space Complexity: O(1)
