e9e46e72231f4c71a89f6aadc5b8ce70302a7f97
olayemii <donyemisco@gmail.com>
2021-12-26 12:03:49 +0100

    Solved 237. Delete Node in a Linked List

---
+++ easy/linkedlist/deletenode/deletenode.go
@@ -0,0 +1,58 @@
+/*
+237. Delete Node in a Linked List
+
+https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list, instead you will be given access to the node to be deleted directly.
+
+It is guaranteed that the node to be deleted is not a tail node in the list.
+
+
+
+Example 1:
+
+
+Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+Example 2:
+
+
+Input: head = [4,5,1,9], node = 1
+Output: [4,5,9]
+Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
+
+
+Constraints:
+
+The number of the nodes in the given list is in the range [2, 1000].
+-1000 <= Node.val <= 1000
+The value of each node in the list is unique.
+The node to be deleted is in the list and is not a tail node
+*/
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteNode(node *ListNode) {
+	for node.Next.Next != nil {
+		node.Val = node.Next.Val
+		node = node.Next
+	}
+	node.Val = node.Next.Val
+	node.Next = nil
+}
+
+// Space Complexity: O(1)
+// Time Complexity: O(n)
