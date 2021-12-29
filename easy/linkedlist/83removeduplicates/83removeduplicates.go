58add329d0c5385eb40a08dc1abf7879daf32adb
olayemii <donyemisco@gmail.com>
2021-12-30 00:42:26 +0100

    Solved 83. Remove Duplicates from Sorted List

---
+++ easy/linkedlist/83removeduplicates/83removeduplicates.go
@@ -0,0 +1,60 @@
+/*
+83. Remove Duplicates from Sorted List
+
+https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+
+
+Example 1:
+
+
+Input: head = [1,1,2]
+Output: [1,2]
+Example 2:
+
+
+Input: head = [1,1,2,3,3]
+Output: [1,2,3]
+
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 300].
+-100 <= Node.val <= 100
+The list is guaranteed to be sorted in ascending order.
+*/
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return head
+	}
+	ans := head
+	for head.Next != nil {
+		if head.Val == head.Next.Val {
+			head.Next = head.Next.Next
+		} else {
+			head = head.Next
+		}
+	}
+
+	return ans
+}
+
+// Space Complexity: O(1)
+// Time Complexity: O(n)
