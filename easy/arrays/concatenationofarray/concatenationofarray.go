cfb68d9fd4a0f52fc260524de19212d48cd6d69a
olayemii <donyemisco@gmail.com>
2022-01-15 23:05:38 +0100

    Solved 1431. Kids With the Greatest Number of Candies

--- easy/arrays/concatenationofarray/concatenationofarray.go
+++ easy/arrays/concatenationofarray/concatenationofarray.go
@@ -1,14 +1,8 @@
 /*
 1929. Concatenation of Array
-Easy
 
-623
+https://leetcode.com/problems/concatenation-of-array/
 
-147
-
-Add to List
-
-Share
 Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
 
 Specifically, ans is the concatenation of two nums arrays.
@@ -44,3 +38,6 @@ package main
 func getConcatenation(nums []int) []int {
 	return append(nums, nums...)
 }
+
+// Space Complexity: O(n)
+// Time Complexity: O(1)
